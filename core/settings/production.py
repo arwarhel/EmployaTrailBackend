76ed7dffbc2dcb25bbe7f42db9ab87769afad39f
Arwa <arwa@localhost.localdomain>
2023-07-28 18:46:43 +0530

    chore(deploy): update openshift deployed url

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,8 +6,9 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['employatrail.pythonanywhere.com',
-                 'employa-trail-backend-route-ans2sqb-dev.apps.sandbox-m3.1530.p1.openshiftapps.com']
+                 'employatrailbackend-arwa-rhel-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com']
 
 CORS_ALLOWED_ORIGINS = [
     "https://employatrail.pythonanywhere.com",
+    "https://employatrailbackend-arwa-rhel-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com"
 ]
